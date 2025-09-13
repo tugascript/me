65f90766d6c100afaa93bfa7b01d971d977f93a2
Afonso Barracha <barracha.afonso@gmail.com>
2025-09-13 13:30:55 +1200

    feat: add entities

---
+++ entity/src/lib.rs
@@ -0,0 +1,35 @@
+// Copyright (c) 2025 Afonso Barracha
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+pub mod prelude;
+
+pub mod auth_provider;
+pub mod blog_post;
+pub mod blog_post_comment;
+pub mod blog_post_comment_reaction;
+pub mod blog_post_image;
+pub mod blog_post_reaction;
+pub mod blog_post_tag;
+pub mod blog_post_technology;
+pub mod blog_series;
+pub mod blog_series_post;
+pub mod blog_series_technology;
+pub mod certificate;
+pub mod common_tag;
+pub mod education;
+pub mod experience;
+pub mod open_source_contribution;
+pub mod project;
+pub mod project_image;
+pub mod project_link;
+pub mod project_technology;
+pub mod related_technology;
+pub mod sea_orm_active_enums;
+pub mod skill;
+pub mod summary;
+pub mod technology;
+pub mod user;
+pub mod user_suspension;
